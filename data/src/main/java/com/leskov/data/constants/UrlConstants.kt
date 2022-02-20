06296e918cd5db6e843dddc16e879728912e6bc1
Sergey Leskov <madarthas445@gmail.com>
2022-02-20 03:16:11 +0300

    Data module

---
+++ data/src/main/java/com/leskov/data/constants/UrlConstants.kt
@@ -0,0 +1,9 @@
+package com.leskov.data.constants
+
+object UrlConstants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val MOVIES = "movie"
+    const val POPULAR_MOVIES = "movie/popular"
+
+}
